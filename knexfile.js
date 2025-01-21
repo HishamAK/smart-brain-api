c29c4361b646c7eee452b7683de46c86c6d920e2
Hisham alkhellow <hishamalkhellow@gmail.com>
2025-01-21 15:03:54 +0300

    update

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
       host: 'pg-654eb1f-fathomless-headland-49488.c.aivencloud.com',
       port: 27913,
       user: 'avnadmin',
-      password: '',
+      password: 'AVNS_Lw2CxF_zj9Id-WA6xp9',
       database: 'defaultdb',
       ssl: {
         rejectUnauthorized: false // Recommended for Heroku deployments
